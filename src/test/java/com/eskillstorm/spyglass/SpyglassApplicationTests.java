ee75cd33a04c26aa5d41a7a54e433beeffc12aa2
h-corbin <macbookpro@Guys-MacBook-Pro.local>
2022-08-29 11:27:10 -0400

    initialize project

---
+++ src/test/java/com/eskillstorm/spyglass/SpyglassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eskillstorm.spyglass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpyglassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
