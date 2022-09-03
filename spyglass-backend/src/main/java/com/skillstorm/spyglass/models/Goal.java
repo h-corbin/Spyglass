66eb66a29b0b3e88930f62526fff869e3f5a2e77
h-corbin <macbookpro@Guys-MacBook-Pro.local>
2022-09-03 10:41:46 -0400

    update models

--- spyglass-backend/src/main/java/com/skillstorm/spyglass/models/Goal.java
+++ spyglass-backend/src/main/java/com/skillstorm/spyglass/models/Goal.java
@@ -45,4 +45,88 @@ public class Goal {
 	
 	@ManyToMany(mappedBy = "goals")
 	private Set<User> users;
+
+	
+	public Goal() {
+		super();
+	}
+
+	public Goal(@NotBlank String name, String description, String picture, @NotNull LocalDate targetDate,
+			@NotNull @DecimalMin("0.0") @Digits(integer = 10, fraction = 2) BigDecimal targetAmount,
+			@NotNull @DecimalMin("0.0") @Digits(integer = 10, fraction = 2) double currentAmount, Set<User> users) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.picture = picture;
+		this.targetDate = targetDate;
+		this.targetAmount = targetAmount;
+		this.currentAmount = currentAmount;
+		this.users = users;
+	}
+
+	public Long getGoalId() {
+		return goalId;
+	}
+
+	public void setGoalId(Long goalId) {
+		this.goalId = goalId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPicture() {
+		return picture;
+	}
+
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+
+	public LocalDate getTargetDate() {
+		return targetDate;
+	}
+
+	public void setTargetDate(LocalDate targetDate) {
+		this.targetDate = targetDate;
+	}
+
+	public BigDecimal getTargetAmount() {
+		return targetAmount;
+	}
+
+	public void setTargetAmount(BigDecimal targetAmount) {
+		this.targetAmount = targetAmount;
+	}
+
+	public double getCurrentAmount() {
+		return currentAmount;
+	}
+
+	public void setCurrentAmount(double currentAmount) {
+		this.currentAmount = currentAmount;
+	}
+
+	public Set<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Set<User> users) {
+		this.users = users;
+	}
+	
+	
 }
--- spyglass-backend/src/main/java/com/skillstorm/spyglass/models/User.java
+++ spyglass-backend/src/main/java/com/skillstorm/spyglass/models/User.java
@@ -42,4 +42,80 @@ public class User {
 			  joinColumns = @JoinColumn(name = "username"), 
 			  inverseJoinColumns = @JoinColumn(name = "goal_id"))
 	private Set<Goal> goals;
+	
+	
+
+	public User() {
+		super();
+	}
+
+	public User(String username, @NotBlank String password, @NotNull boolean enabled, @NotBlank String firstName,
+			@NotBlank String lastName, @NotBlank @Email String email, Set<Goal> goals) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.enabled = enabled;
+		this.firstName = firstName;
+		LastName = lastName;
+		this.email = email;
+		this.goals = goals;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return LastName;
+	}
+
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Set<Goal> getGoals() {
+		return goals;
+	}
+
+	public void setGoals(Set<Goal> goals) {
+		this.goals = goals;
+	}
+	
+	
 }
