70c51ced0faee67ebc1c11b9a7858aadde6fcb30
h-corbin <macbookpro@Guys-MacBook-Pro.local>
2022-09-08 14:12:06 -0400

    update CORS filter

--- spyglass-backend/src/main/java/com/skillstorm/spyglass/filters/CorsFilter.java
+++ spyglass-backend/src/main/java/com/skillstorm/spyglass/filters/CorsFilter.java
@@ -18,10 +18,10 @@ public class CorsFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, HEAD");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization,  cache-control, content-type, Origin, key");
+        response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Content-Type", "*");
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
